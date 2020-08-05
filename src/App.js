836c0466bec0959ef38af99008985ca5d292c474
Ruslan <almostz@yandex.ru>
2020-08-05 04:01:17 +0400

    feat: search

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Pagination from './components/Pagination'
         
         [x] Сортировка по колонкам
 
-        [ ] Поиск, обновлять результаты по нажатию кнопки "Найти"
+        [x] Поиск, обновлять результаты по нажатию кнопки "Найти"
 
         [x] Выводить дополнительную информацию по нажатию на строку таблицы
 
@@ -20,19 +20,21 @@ import Pagination from './components/Pagination'
             [ ] После заполнения всех инпутов активируется кнопка "Добавить в таблицу", вставляет ряд в НАЧАЛО таблицы
 */
 
-const MAX_IN_PAGE = 5
+const MAX_IN_PAGE = 50
 
 function App() {
 
     const [data, setData] = useState([])
+    const [searchResult, setSearchResult] = useState(null)
     const [users, setUsers] = useState(Array.from(Array(MAX_IN_PAGE), () => 0))
     const [user, setUser] = useState({})
 
     useEffect(() => {
-        const url = 'http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}'
+        const url = 'http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}'
         fetch(url)
             .then(res => res.json())
             .then(res => {
+                console.log(res)
                 setData(res)
             })
             .catch(err => { alert('An error occured. Please try to refresh the page') })
@@ -41,16 +43,16 @@ function App() {
     const getPage = useCallback((page) => {
         const begin = (page - 1) * MAX_IN_PAGE
         const end = page * MAX_IN_PAGE
-        let newUsers = data.slice(begin, end)
+        let newUsers = searchResult === null ? data.slice(begin, end) : searchResult.slice(begin, end)
         while (newUsers.length < MAX_IN_PAGE) {
             newUsers.push({ animation: false })
         }
         // чтоб не изменялся размер таблицы
         setUsers(newUsers)
-    }, [data])
+    }, [data, searchResult])
 
     const onSort = (id, ascending) => {
-        setData([...data].sort((a, b) => {
+        const sortFn = (a, b) => {
             const aProp = a[Object.keys(a)[id]]
             const bProp = b[Object.keys(b)[id]]
             if (aProp > bProp)
@@ -58,7 +60,25 @@ function App() {
             if (bProp > aProp)
                 return ascending ? -1 : 1
             return 0
-        }))
+        }
+        setData([...data].sort(sortFn))
+        if (searchResult !== null)
+            setSearchResult([...searchResult].sort(sortFn))
+    }
+
+    const onSearch = (query) => {
+        if (query.replace(/ /g, '') === '')
+            setSearchResult(null)
+        else {
+            const result = data.filter((user) =>
+                user.id.toString().toLowerCase().includes(query.toLowerCase()) ||
+                user.firstName.toLowerCase().includes(query.toLowerCase()) ||
+                user.lastName.toLowerCase().includes(query.toLowerCase()) ||
+                user.email.toLowerCase().includes(query.toLowerCase()) ||
+                user.phone.toLowerCase().includes(query.toLowerCase())
+            )
+            setSearchResult(result)
+        }
     }
 
     useEffect(() => {
@@ -67,8 +87,8 @@ function App() {
 
     return (
         <div className='App container'>
-            <Table data={users} setUser={setUser} onSort={onSort} />
-            {users[0] ? <Pagination lastPage={Math.ceil(data.length / MAX_IN_PAGE)} getPage={getPage} /> : null}
+            <Table data={users} setUser={setUser} onSort={onSort} onSearch={onSearch} />
+            {users[0].id ? <Pagination lastPage={searchResult === null ? Math.ceil(data.length / MAX_IN_PAGE) : Math.ceil(searchResult.length / MAX_IN_PAGE)} getPage={getPage} /> : null}
             { // вынести в компонент
                 user.id ?
                     <div>
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Search from './Search'
 import { Skeleton } from '@material-ui/lab'
 
 const Table = (props) => {
@@ -16,43 +17,46 @@ const Table = (props) => {
     }
 
     return (
-        <table className='table table-striped col-12 border p-3 mt-3'>
-            <thead>
-                <tr className='d-flex user-select-none'>
-                    <th className='col-2' onClick={() => onHeadClick(0)}>Id {sortBy.id === 0 ? sortBy.ascending ? '▲' : '▼' : null}</th>
-                    <th className='col-2' onClick={() => onHeadClick(1)}>Name {sortBy.id === 1 ? sortBy.ascending ? '▲' : '▼' : null}</th>
-                    <th className='col-2' onClick={() => onHeadClick(2)}>Last Name {sortBy.id === 2 ? sortBy.ascending ? '▲' : '▼' : null}</th>
-                    <th className='col-3' onClick={() => onHeadClick(3)}>Email {sortBy.id === 3 ? sortBy.ascending ? '▲' : '▼' : null}</th>
-                    <th className='col-3' onClick={() => onHeadClick(4)}>Phone {sortBy.id === 4 ? sortBy.ascending ? '▲' : '▼' : null}</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    props.data.map((user, idx) =>
-                        <tr key={idx} className='d-flex' onClick={() => {
-                            if (user.id) props.setUser(user)
-                        }}>
-                            <td className='col-2'>
-                                {user.id ? user.id : <Skeleton animation={user.animation} />}
-                            </td>
-                            <td className='col-2'>
-                                {user.firstName ? user.firstName : <Skeleton animation={user.animation} />}
-                            </td>
-                            <td className='col-2'>
-                                {user.lastName ? user.lastName : <Skeleton animation={user.animation} />}
-                            </td>
+        <React.Fragment>
+            <Search onSearch={props.onSearch} />
+            <table className='table table-striped col-12 border p-3 mt-3'>
+                <thead>
+                    <tr className='d-flex user-select-none'>
+                        <th className='col-2' onClick={() => onHeadClick(0)}>Id {sortBy.id === 0 ? sortBy.ascending ? '▲' : '▼' : null}</th>
+                        <th className='col-2' onClick={() => onHeadClick(1)}>Name {sortBy.id === 1 ? sortBy.ascending ? '▲' : '▼' : null}</th>
+                        <th className='col-2' onClick={() => onHeadClick(2)}>Last Name {sortBy.id === 2 ? sortBy.ascending ? '▲' : '▼' : null}</th>
+                        <th className='col-3' onClick={() => onHeadClick(3)}>Email {sortBy.id === 3 ? sortBy.ascending ? '▲' : '▼' : null}</th>
+                        <th className='col-3' onClick={() => onHeadClick(4)}>Phone {sortBy.id === 4 ? sortBy.ascending ? '▲' : '▼' : null}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        props.data.map((user, idx) =>
+                            <tr key={idx} className='d-flex' onClick={() => {
+                                if (user.id) props.setUser(user)
+                            }}>
+                                <td className='col-2'>
+                                    {user.id !== (null || undefined) ? user.id : <Skeleton animation={user.animation} />}
+                                </td>
+                                <td className='col-2'>
+                                    {user.firstName !== (null || undefined) ? user.firstName : <Skeleton animation={user.animation} />}
+                                </td>
+                                <td className='col-2'>
+                                    {user.lastName !== (null || undefined) ? user.lastName : <Skeleton animation={user.animation} />}
+                                </td>
 
-                            <td className='col-3'>
-                                {user.email ? user.email : <Skeleton animation={user.animation} />}
-                            </td>
-                            <td className='col-3'>
-                                {user.phone ? user.phone : <Skeleton animation={user.animation} />}
-                            </td>
-                        </tr>
-                    )
-                }
-            </tbody>
-        </table>
+                                <td className='col-3'>
+                                    {user.email !== (null || undefined) ? user.email : <Skeleton animation={user.animation} />}
+                                </td>
+                                <td className='col-3'>
+                                    {user.phone !== (null || undefined) ? user.phone : <Skeleton animation={user.animation} />}
+                                </td>
+                            </tr>
+                        )
+                    }
+                </tbody>
+            </table>
+        </React.Fragment>
     )
 }
 
