e9aa284b58ee38366c4c5fa4ad9e96bae6488343
Ruslan <almostz@yandex.ru>
2020-08-10 05:39:08 +0400

    style: remove useless imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState, useEffect } from 'react'
 import Table from './components/Table'
-import Pagination from './components/Pagination'
 
 /* 
     Todo:
